3105758e1bd29d4a2c451169c2e661a28dfed1d1
ArjunTippu91 <80086496+ArjunTippu91@users.noreply.github.com>
2024-12-18 12:26:17 +0530

    Update dummy_test.py

--- dummy_test.py
+++ dummy_test.py
@@ -1,5 +1,9 @@
+'''This is simple unit test comparison
+'''
 def capital_case(x):
     return x.capitalize()
 
+'''Assertion
+'''
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
